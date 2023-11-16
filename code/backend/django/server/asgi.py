6e0c0e46b0392cea9cd4ef0f82c11ef439c91ac5
sgrunf <davide.luccioli@studio.unibo.it>
2023-11-16 13:29:47 +0100

    small correction

--- code/backend/django/server/asgi.py
+++ code/backend/django/server/asgi.py
@@ -11,8 +11,11 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
+from api.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 

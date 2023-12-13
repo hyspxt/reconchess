7315250649abbf6839a2caf03f327c044d560379
giulia-t <giulia.torsani@studio.unibo.it>
2023-12-13 06:53:24 -0800

    testing print leaderboard

--- code/backend/django/api/tables_interactions.py
+++ code/backend/django/api/tables_interactions.py
@@ -41,11 +41,14 @@ async def get_leaderboard():
 
     leaderboard = []
     for player in players_data:
+        player_name = player['player_name']
+        player_stats = await get_player_loc_stats(player_name)
+
         leaderboard.append({
-            'player_name': player['player_name'],
-            'wins': player['wins'],
-            'draws': player['draws'],
-            'losses': player['losses'],
+            'player_name': player_name,
+            'wins': player_stats['n_wins'],
+            'draws': player_stats['n_draws'],
+            'losses': player_stats['n_lost'],
         })
 
     return leaderboard

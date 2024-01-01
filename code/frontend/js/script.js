6914f0bd6165d4df11898b8be979191cc0dcd5ce
sgrunf <davide.luccioli@studio.unibo.it>
2024-01-01 13:12:45 +0100

    fixed some issues with login and registration

--- code/frontend/js/script.js
+++ code/frontend/js/script.js
@@ -1,14 +1,3 @@
-export function generaStringaCasuale(length) {
-    const permittedChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let string = '';
-  
-    for (let i = 0; i < length; i++) {
-      const casualIndex = Math.floor(Math.random() * permittedChar.length);
-      string += permittedChar.charAt(casualIndex);
-    }
-    return string;
-  }
- 
 document.addEventListener("DOMContentLoaded", function() {
     var element = document.getElementById("not_signed_ini6gdrs9oeic0");
     if(element) {
@@ -35,73 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-
-function handleRegistration(event) {
-    event.preventDefault();
-    //get form data
-    const formData = new FormData(event.target);
-    //get csfttoken value from cookie
-
-    fetch("/api/register/", {
-        method: "POST",
-        body: formData,
-    }).then(response => {
-        const alert = document.createElement("div");
-        alert.classList.add("alert", "mt-3", "text-center");
-
-        if (response.ok) {
-            alert.classList.add("alert-success");
-        } else {
-            alert.classList.add("alert-danger");
-        }
-
-        response.text().then(data => {
-            alert.innerHTML = data;
-        });
-
-        const nextElement = event.target.nextElementSibling;
-        if (nextElement.classList.contains("alert")) {
-            nextElement.remove();
-        }
-        event.target.after(alert);
-    })
-    .catch(error => {
-        console.error(error);
-    });
-}
-
-
-function handleLogin(event) {
-    event.preventDefault();
-
-    //get form data
-    const formData = new FormData(event.target);
-    //get csfttoken value from cookie
-
-    fetch("/api/login/", {
-        method: "POST",
-        body: formData,
-    }).then(response => {
-        const alert = document.createElement("div");
-        alert.classList.add("alert", "mt-3", "text-center");
-
-        if (response.ok) {
-            alert.classList.add("alert-success");
-        } else {
-            alert.classList.add("alert-danger");
-        }
-
-        response.text().then(data => {
-            alert.innerHTML = data;
-        });
-
-        const nextElement = event.target.nextElementSibling;
-        if (nextElement.classList.contains("alert")) {
-            nextElement.remove();
-        }
-        event.target.after(alert);
-    })
-}
 
 function handleCredentialResponse(response) {
     var id_token = response.credential;

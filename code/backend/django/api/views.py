40147b736e04f080923f3b697159cc19bb899eb4
Kmoon7 <kaori.jiang@studio.unibo.it>
2024-01-07 03:13:15 +0100

    fix login

--- code/backend/django/api/views.py
+++ code/backend/django/api/views.py
@@ -11,7 +11,7 @@ from .models import Users
 from django.utils import timezone
 import json
 
-#TODO: install the 'google-auth' library
+SESSION_KEY = 'user_id'
 
 @csrf_exempt
 def register(request):
@@ -52,14 +52,19 @@ def userLogin(request):
 @csrf_exempt
 def userLogout(request):
 	logout(request)
+	if  SESSION_KEY in request.session:
+		del request.session[SESSION_KEY]
 	return HttpResponse('User logged out successfully!', content_type='text/plain')
 
 #cheks if user a user is currently logged in returns the username if so
 def checkLogin(request):
-    if request.user.is_authenticated:
-        return JsonResponse({'loggedIn': True, 'username': request.user.username})
-    else:
-        return JsonResponse({'loggedIn': False})
+	if request.user.is_authenticated:
+		return JsonResponse({'loggedIn': True, 'username': request.user.username})
+	elif SESSION_KEY in request.session:
+		user = User.objects.get(id=request.session[SESSION_KEY])
+		return JsonResponse({'loggedIn': True, 'username': user.username})
+	else:
+		return JsonResponse({'loggedIn': False})
     
 #verification of google id token
 @csrf_exempt
@@ -73,22 +78,25 @@ def googleID(request):
 		# Verify the ID Token
 		id_info = id_token.verify_oauth2_token(id_token_string, requests.Request(), client_id)
 		user_email = id_info.get('email')
-		user_name = id_info.get('name')
+		username = generate_username(user_email)
 
 		# Check if the user with the given email already exists in your database
-		user, created = User.objects.get_or_create(email=user_email, defaults={'username': user_email})
+		user, created = User.objects.get_or_create(email=user_email, defaults={'username': username})
 		
-		user = authenticate(request, username=user.email)
-		if user is not None:
-			login(request, user)
-		else:
-			# Handle case where user could not be authenticated
-			return JsonResponse({'success': False, 'error': 'Could not authenticate user'})
-
-		return JsonResponse({'success': True, 'user_email': user_email, 'user_name': user_name})
+        # Manually log in the user by setting the user in the request's session
+		request.session[SESSION_KEY] = user.id
+        
+		return JsonResponse({'success': True, 'user_email': user_email, 'user_name': username})
 
 	except ValueError as e:
 		return JsonResponse({'success': False, 'error': str(e)})
+
+def generate_username(email):
+	username = email.split('@')[0]
+	count = User.objects.filter(username__startswith=username).count()
+	if count:
+		username = '{}{}'.format(username, count + 1)
+	return username
 
 def player_loc_stats(request, player_email):
 	result = ti.get_player_loc_stats(player_email)
